1078ecb56d54fe065740cbb7f0eef29aaef26c43
lucianodiaz <lucianodiazberberich@gmail.com>
2020-07-05 18:19:48 -0300

    Agregado de boton Spin
    Se agrega el grafico de boton en la escena con su inputEnabled para detectar cuando se toca el boton.

---
+++ src/bootloader.js
@@ -0,0 +1,28 @@
+class Bootloader extends Phaser.Scene
+{
+    constructor()
+    {
+        super({key: "Bootloader"});
+    }
+
+    preload()
+    {
+        this.load.on("complete",() => 
+        {
+            this.scene.start("Scn_play");
+        });
+        
+        //this.load.image("button","./assets/btn_spin.png");
+        this.load.image("backFrame","./assets/frame.png");
+        this.load.image("line1","./assets/line_1.png");
+        this.load.image("line4","./assets/line_4.png");
+        this.load.image("line5","./assets/line_5.png");
+        this.load.image("logo","./assets/logo_mobile.png");
+        this.load.image("prizeWindow","./assets/prize_window.png");
+        this.load.image("button", "./assets/btn_spin.png");
+
+        
+    }
+}
+
+export default Bootloader;
