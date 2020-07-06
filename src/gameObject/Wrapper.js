f67b979f9fbd52a83602eb800fcd1e927ddbe4e6
lucianodiaz <lucianodiazberberich@gmail.com>
2020-07-05 21:00:34 -0300

    Logica Puntos
    Se agrego la logica de los puntos y la posibilidad de llamar a spin desde otra escena y usar los datos que trae consigo

---
+++ src/gameObject/Wrapper.js
@@ -0,0 +1,221 @@
+class wrapper 
+{
+    REELS = [];
+    PAYLINES = [];
+    PAYTABLE = [];
+
+    LAYOUT_COLS = 3;
+    LAYOUT_ROWS = 3;
+//DATA
+constructor()
+{
+    this.REELS = [
+		['b', 'b', 'c', 'b', 'a', 'd', 'e', 'd', 'a', 'a', 'e', 'b', 'a', 'd', 'c', 'c', 'e', 'e', 'c', 'd'],
+		['d', 'b', 'e', 'e', 'e', 'b', 'c', 'b', 'a', 'd', 'd', 'e', 'a', 'a', 'b', 'c', 'c', 'd', 'c', 'a'],
+		['d', 'c', 'a', 'e', 'c', 'a', 'c', 'b', 'a', 'e', 'd', 'e', 'a', 'b', 'b', 'e', 'b', 'd', 'c', 'd']
+	];
+
+	/*
+		CELL POSITIONS
+		0 1 2
+		3 4 5
+		6 7 8
+	*/
+	this.PAYLINES = [
+		[3, 4, 5],
+		[0, 1, 2],
+		[6, 7, 8],
+		[0, 4, 8],
+		[6, 4, 2]
+	];
+
+	this.PAYTABLE = [
+		{ symbol: "a", prize:50 },
+		{ symbol: "b", prize:45 },
+		{ symbol: "c", prize:40 },
+		{ symbol: "d", prize:35 },
+		{ symbol: "e", prize:30}
+	];
+}
+getReels = function()
+{
+	return this.REELS;
+}
+
+getPaylines = function()
+{
+	return this.PAYLINES;
+}
+
+getPaytable = function()
+{
+	return this.PAYTABLE;
+}
+
+//ACTIONS
+
+spin = function()
+{
+	var stop_points = this.getRandomStops();
+	var prizes = this.getPrizes(stop_points);
+	var result = this.getResultData(stop_points, prizes);
+
+	return result;
+}
+
+getRandomStops = function()
+{
+	var sp = new Array();
+	var rnd;
+	for(var i = 0; i < this.LAYOUT_COLS; i++)
+	{
+		rnd = Math.floor(Math.random() * this.REELS[i].length);
+		sp.push(rnd);
+	}
+
+	//return [0, 0, 0];
+	return sp;
+}
+
+getPrizes = function(sp)
+{
+	var layout = this.getGridLayout(sp);
+	var prizes = [];
+	var prize = null;
+	for(var i = 0; i < this.PAYLINES.length; i++)
+	{
+		prize = this.getLinePrize(i, layout);
+		if(prize != null)
+		{
+			prizes.push( prize );
+		}
+	}
+
+	return prizes;
+}
+
+getResultData = function(sp, prizes)
+{
+	var result = {};
+	result.stopPoints = sp;
+	result.layout = this.getGridLayout(sp);
+	result.reelsLayout = [ 
+		this.getReelLayout(0, sp[0]),
+		this.getReelLayout(1, sp[1]),
+		this.getReelLayout(2, sp[2])
+	];
+	result.prizes = prizes;
+	result.winnings = 0;
+	for(var i = 0; i < prizes.length; i++)
+	{
+		result.winnings += prizes[i].winnings;
+	}
+
+	return result;
+}
+
+getGridLayout = function(sp)
+{
+	var grid = [];
+	var reel = null;
+	for(var i = 0; i < this.LAYOUT_COLS; i++)
+	{
+		reel = this.getReelLayout(i, sp[i]);
+		grid.push(reel);
+	}
+
+	var layout = [];
+	var col = 0;
+	var row = 0;
+	var symId = null;
+	while(row < this.LAYOUT_ROWS)
+	{
+		symId = grid[col][row];
+		layout.push(symId);
+
+		col++;
+		if(col == this.LAYOUT_COLS)
+		{
+			col = 0;
+			row++;
+		}
+	}
+
+	return layout;
+}
+
+getReelLayout = function(reelId, sp)
+{
+	var reel = this.REELS[reelId];
+	var index = sp;
+	var symId = null;
+	var layout = [];
+
+	for(var i = 0; i < this.LAYOUT_ROWS; i++)
+	{
+		if(index >= reel.length) { index = 0; }
+		var symId = reel[index];
+		layout.push( symId );
+
+		index++;
+	}
+
+	return layout;
+}
+
+getLinePrize = function(lineId, layout)
+{
+	var line = this.PAYLINES[lineId];
+	var firstSymId = layout[ line[0] ];
+	
+	var	hasPrize = true;
+	var symPos = null;
+	for(var i = 1; i < line.length; i++)
+	{
+		symPos = line[i];
+		if(layout[symPos] != firstSymId)
+		{
+			hasPrize = false;
+			break;
+		}
+	}
+
+	var prize = null;
+	if(hasPrize)
+	{
+		prize = {};
+		prize.lineId = lineId;
+		prize.symId = firstSymId;
+		prize.winnings = this.getSymbolPrize(firstSymId);
+	}
+	
+	return prize;
+}
+
+getSymbolPrize = function(symId)
+{
+	var symConfig = null;
+	var prize = 0;
+	for(var i = 0; i < this.PAYTABLE.length; i++)
+	{
+		symConfig = this.PAYTABLE[i];
+		if(symId == symConfig.symbol)
+		{
+			prize = symConfig.prize;
+		}
+	}
+
+	return prize;
+}
+
+// wrapper = new Wrapper();
+// console.log(wrapper.getReels());
+// console.log(wrapper.getPaylines());
+// console.log(wrapper.getPaytable());
+// console.log(wrapper.spin());
+
+
+}
+
+
+export default wrapper;
--- src/scenes/scn_play.js
+++ src/scenes/scn_play.js
@@ -1,5 +1,8 @@
+import wrapper from '../gameObject/Wrapper.js'
 class Scn_play extends Phaser.Scene
 {
+    wrapper = new wrapper();
+
     constructor()
     {
         super({key: "Scn_play"});
@@ -7,29 +10,50 @@ class Scn_play extends Phaser.Scene
 
     create()
     {
+        //Wrapper
+        console.log(this.wrapper.getReels());
+        console.log(this.wrapper.getPaylines());
+        console.log(this.wrapper.getPaytable());
 
+        
+
+        //logo,back,boton,puntos
         this.logo = this.add.image(this.sys.game.config.width/2,5,"logo");
         this.backframe = this.add.image(0,80,"backFrame");
+        this.btnSpin = this.add.sprite(this.sys.game.config.width/2 + 150,this.sys.game.config.height - 50,"button");
+        this.points = this.add.image(95,this.sys.game.config.height - 50,'prizeWindow');
+        this.textPoints = this.add.text(this.points.x,this.points.y,'0',
+        {
+            fontSize : 27,
+            align : 'center',
+            color : '#263e71',
+            fontStyle : 'bold'
+        });
+
+
 
         this.logo.setOrigin(0.5,0);
         this.backframe.setOrigin(0,0);
-
-        this.btnSpin = this.add.sprite(this.sys.game.config.width/2 + 150,this.sys.game.config.height - 50,"button");
         this.btnSpin.setInteractive();
-        
-        this.btnSpin.on("pointerdown",function()
+        this.textPoints.setOrigin(0.5,0.5);
+     
+        this.registry.events.on('spin',() =>
         {
-            console.log("SPIN!");
+            this.SpinResults = this.wrapper.spin();
+            console.log(this.SpinResults);
+
+            console.log('puntos: ' + this.SpinResults.winnings);
+
+            this.textPoints.setText(this.SpinResults.winnings);
         });
-        this.btnSpin.on("pointerup",function()
+
+        this.btnSpin.on("pointerdown", () =>
         {
-            console.log("up!");
-          
+            this.registry.events.emit('spin');    
         });
     }
-
-    
 }
+
 
 export default Scn_play;
 
